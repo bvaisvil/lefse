1a17ec897c07a2106dd3bee6d3c4066b8e403244
Benjamin Vaisvil <bvaisvil@ignebio.com>
2021-02-24 18:36:02 -0600

    - remove the embedded font

--- plot_cladogram.py
+++ plot_cladogram.py
@@ -292,7 +292,7 @@ def draw_tree(out_file,tree,params):
         seps = [params['clade_sep']*sep/(float(depth-i+1)*0.25) for i in range(1,len(tree['nlev'])+1)]
         totseps = sum([s*nlev[i] for i,s in enumerate(seps[:-1])])
     if clade_sep_err: print('clade_sep parameter too large, lowered to',params['clade_sep'])
-    #plt.rcParams['svg.fonttype'] = 'none'
+    plt.rcParams['svg.fonttype'] = 'none'
     fig = plt.figure(edgecolor=params['back_color'], facecolor=params['back_color'])
     ax = fig.add_subplot(111, polar=True, frame_on=False, facecolor=params['back_color'] )
     #plt.subplots_adjust(right=1.0-params['r_prop'],left=params['l_prop'])
